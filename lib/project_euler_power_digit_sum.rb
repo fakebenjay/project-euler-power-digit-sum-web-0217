c413e00d1879089e8adc1d31e7c6305a4f9d0825
fakebenjay <fakebenjay@gmail.com>
2017-05-21 12:36:49 -0400

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,9 @@
-# code your solution here
+def power_digit_sum(x, n)
+  y = x ** n
+
+  why = y.to_s.split('')
+
+  num_array = why.map { |y| y.to_i }
+
+  num_array.inject { |sum, n| sum + n }
+end
